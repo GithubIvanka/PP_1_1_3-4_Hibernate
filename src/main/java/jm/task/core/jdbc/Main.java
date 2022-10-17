409423a480f4808158375d2e4a852a3f453cab26
GithubIvanka <ivanivanka913@gmail.com>
2022-10-17 11:51:48 +0700

    hibernate update

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -6,6 +6,7 @@ import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
 
 import java.util.List;
 
@@ -28,7 +29,6 @@ public class Main {
         }
         us.cleanUsersTable();
         us.dropUsersTable();
-
-
+        Util.closeSessionFactory();
     }
 }
